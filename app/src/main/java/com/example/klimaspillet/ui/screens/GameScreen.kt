9892479bfbb5822ab1d1e341cc7173c085544663
vilo0001 <vilo0001@stud.kea.dk>
2025-05-20 05:40:59 +0200

    Bedre Red/Yellow valg design. Back button ikon og placering. Padding mellem gul og rød knapper.

--- app/src/main/java/com/example/klimaspillet/ui/screens/GameScreen.kt
+++ app/src/main/java/com/example/klimaspillet/ui/screens/GameScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,11 +16,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -35,6 +35,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.klimaspillet.R
+import androidx.compose.material3.IconButton
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.zIndex
 
 //   ------------------------------------
 //   Hovedsageligt ansvarlig: Victor Lotz
@@ -56,13 +60,6 @@ fun GameScreen (navController: NavController) {
 
         // Middleground I guess ??? :D   Det er scoren bag content og back button.
         Score()
-        Box() {
-            Button(onClick = {
-
-            }) {
-                Text("<")
-            }
-        }
 
         // Content
         Column(
@@ -70,10 +67,9 @@ fun GameScreen (navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(24.dp)
-                .padding(bottom = 50.dp)
+                .padding(bottom = 70.dp)
         ) {
-            Title()
+            BackButtonAndTitle()
             CO2Choices()
             RedAndYellowButtons()
         }
@@ -83,38 +79,58 @@ fun GameScreen (navController: NavController) {
 @Preview
 @Composable
 fun Preview() {
-    Box(modifier = Modifier
-        .background(color = Color(0xFFFFCA58))
-    ) {
-        Text("Hello :D")
-        Box(
-            modifier = Modifier
-                .offset(x = -65.dp)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.hakket_oksekoed),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(130.dp)
-                    .clip(CircleShape)
-                    .border(
-                        BorderStroke(5.dp, Color(0xFFFFCA58)),
-                        CircleShape
-                    )
-            )
-        }
-    }
+    CO2Choices()
 }
 
+
 @Composable
-fun Title() {
-    Text("Hvad udleder mest CO2?",
+fun BackButtonAndTitle() {
+    Box(
         modifier = Modifier
-            .fillMaxWidth(0.8f),
-        fontSize = 40.sp,
-        color = Color.White,
-        fontFamily = FontFamily(Font(R.font.bagel_fat_one)),
-        textAlign = TextAlign.Center)
+            .fillMaxWidth()
+            .padding(top = 24.dp)
+            .padding(10.dp)
+    ) {
+        // Back button
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+        ) {
+            IconButton(
+                modifier = Modifier
+                    .size(50.dp)
+                    .align (Alignment.CenterStart),
+                onClick = { }
+            ) {
+                // For at opnå en drop-shadow effekt af et ikon, indsætter jeg det samme ikon to gange, men gør den første mere blurry.
+                Box {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_arrow_back_ios_24),
+                        contentDescription = null,
+                        Modifier
+                            .offset(x = (2).dp, y = (1).dp)
+                            .blur(3.dp),
+                        tint = Color.Gray
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_arrow_back_ios_24),
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+            }
+        }
+
+        // Title
+        Text("Hvad udleder mest CO2?",
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .align(Alignment.Center),
+            fontSize = 40.sp,
+            color = Color.White,
+            fontFamily = FontFamily(Font(R.font.bagel_fat_one)),
+            textAlign = TextAlign.Center)
+    }
 }
 
 @Composable
@@ -135,96 +151,147 @@ fun Score() {
 
 @Composable
 fun CO2Choices() {
-    Box() {
-        Column {
-            Row {
-                Image(
-                    painter = painterResource(id = R.drawable.hakket_oksekoed),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(130.dp)
-                        .clip(CircleShape)
-                        .border(
-                            BorderStroke(5.dp, Color(0xFFFFCA58)),
-                            CircleShape
-                        )
-                )
-                Text("udleder 7,48kg CO2e",
-                    fontSize = 14.sp,
-                    color = Color.White,
-                    fontFamily = FontFamily(Font(R.font.bagel_fat_one)))
-            }
-
+    val placement = 480.dp;
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(placement)
+            .padding(top = placement-350.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .zIndex(10f)
+                .align (Alignment.Center)
+        ) {
             Text("VS",
-                fontSize = 40.sp,
                 color = Color.White,
-                fontFamily = FontFamily(Font(R.font.bagel_fat_one)))
-
-            Row {
-                Text("udleder ???kg CO2e",
-                    fontSize = 14.sp,
-                    color = Color.White,
-                    fontFamily = FontFamily(Font(R.font.bagel_fat_one)))
-                Image(
-                    painter = painterResource(id = R.drawable.kyllingekoed),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(130.dp)
-                        .clip(CircleShape)
-                        .border(
-                            BorderStroke(5.dp, Color(0xFFFF5858)),
-                            CircleShape
-                        )
-                )
-            }
+                fontFamily = FontFamily(Font(R.font.bagel_fat_one)),
+                textAlign = TextAlign.Center,
+                fontSize = 60.sp,
+                modifier = Modifier
+                    .align (Alignment.Center)
+            )
         }
+        YellowOption(modifier = Modifier
+            .align(Alignment.TopEnd), 7.48f)
+        RedOption(modifier = Modifier
+            .align(Alignment.BottomStart), 1.2f)
+    }
+}
 
+@Composable
+fun YellowOption(modifier: Modifier, CO2e: Float) {
+    Box(
+        modifier
+            .clip(shape = RoundedCornerShape(100.dp, 0.dp, 0.dp, 100.dp))
+            .fillMaxWidth(0.8f)
+            .background(Color(0xFFFFCA58))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.hakket_oksekoed),
+            contentDescription = null,
+            modifier = Modifier
+                .size(180.dp)
+                .clip(CircleShape)
+                .border(
+                    BorderStroke(8.dp, Color(0xFFFFCA58)),
+                    CircleShape
+                )
+        )
+        Text("udleder ${CO2e}kg CO2e",
+            fontSize = 12.sp,
+            color = Color.White,
+            fontFamily = FontFamily(Font(R.font.bagel_fat_one)),
+            modifier = Modifier
+                .align (Alignment.TopEnd)
+                .padding(end = 8.dp, top = 2.dp)
+        )
+    }
+}
+
+@Composable
+fun RedOption(modifier: Modifier, CO2e: Float) {
+    val CO2e = CO2e
+    Box(
+        modifier
+            .clip(shape = RoundedCornerShape(0.dp, 100.dp, 100.dp, 0.dp))
+            .fillMaxWidth(0.8f)
+            .background(Color(0xFFFF5858))
+    ) {
+        Text("udleder ???kg CO2e",
+            fontSize = 12.sp,
+            color = Color.White,
+            fontFamily = FontFamily(Font(R.font.bagel_fat_one)),
+            modifier = Modifier
+                .align (Alignment.BottomStart)
+                .padding(start = 8.dp, bottom = 2.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.kyllingekoed),
+            contentDescription = null,
+            modifier = Modifier
+                .size(180.dp)
+                .clip(CircleShape)
+                .border(
+                    BorderStroke(8.dp, Color(0xFFFF5858)),
+                    CircleShape
+                )
+                .align (Alignment.CenterEnd)
+        )
     }
 }
 
 @Composable
 fun RedAndYellowButtons() {
-    Row(
-        horizontalArrangement = Arrangement.Center,
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(60.dp)
+            .padding(10.dp)
     ) {
-        Button(
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFCA58)),
+        Row(
+            horizontalArrangement = Arrangement.Center,
             modifier = Modifier
-                .fillMaxSize().weight(1f),
-            onClick = {
-
-            }
+                .fillMaxWidth()
+                .height(60.dp)
         ) {
-            Row() {
-                Text("IKON")
-                Text(
-                    "Gul",
-                    fontSize = 24.sp,
-                    color = Color.White,
-                    fontFamily = FontFamily(Font(R.font.bagel_fat_one))
-                )
-            }
-        }
+            Button(
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFCA58)),
+                modifier = Modifier
+                    .fillMaxSize().weight(1f)
+                    .padding(end = 10.dp),
+                onClick = {
 
-        Button(
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF5858)),
-            modifier = Modifier
-                .fillMaxSize().weight(1f),
-            onClick = {
-
+                }
+            ) {
+                Row() {
+                    Text("IKON")
+                    Text(
+                        "Gul",
+                        fontSize = 24.sp,
+                        color = Color.White,
+                        fontFamily = FontFamily(Font(R.font.bagel_fat_one))
+                    )
+                }
             }
-        ) {
-            Row() {
-                Text("IKON")
-                Text(
-                    "Rød",
-                    fontSize = 24.sp,
-                    color = Color.White,
-                    fontFamily = FontFamily(Font(R.font.bagel_fat_one))
-                )
+
+            Button(
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF5858)),
+                modifier = Modifier
+                    .fillMaxSize().weight(1f)
+                    .padding(start = 10.dp),
+                onClick = {
+
+                }
+            ) {
+                Row() {
+                    Text("IKON")
+                    Text(
+                        "Rød",
+                        fontSize = 24.sp,
+                        color = Color.White,
+                        fontFamily = FontFamily(Font(R.font.bagel_fat_one))
+                    )
+                }
             }
         }
     }
