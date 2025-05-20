dbe03f220efa88a2e34c01efced6e2b19a92881c
vilo0001 <vilo0001@stud.kea.dk>
2025-05-20 10:44:16 +0200

    ResultsScreen i navigation.

--- app/src/main/java/com/example/klimaspillet/navigation/Navigation.kt
+++ app/src/main/java/com/example/klimaspillet/navigation/Navigation.kt
@@ -7,20 +7,24 @@ import androidx.navigation.compose.rememberNavController
 import com.example.klimaspillet.ui.screens.HomeScreen
 import com.example.klimaspillet.ui.screens.GameScreen
 import com.example.klimaspillet.ui.screens.ConnectClassScreen
+import com.example.klimaspillet.ui.screens.ResultsScreen
 
 
 @Composable
 fun Navigation(){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Routes.routeHomeScreen, builder = {
-        composable(Routes.routeGameScreen){
-            GameScreen(navController)
-        }
         composable(Routes.routeHomeScreen){
             HomeScreen(navController)
         }
+        composable(Routes.routeGameScreen){
+            GameScreen(navController)
+        }
         composable(Routes.routeConnectClassScreen){
             ConnectClassScreen(navController)
+        }
+        composable(Routes.routeResultsScreen){
+            ResultsScreen(navController)
         }
     })
 }
