b2e900c1860429db5914c50206d7cb2de57eb993
AndreasRG <andreas@revesz-gudmann.dk>
2025-05-20 16:05:51 +0200

    Færdiggjort sidste changes ved merge

--- app/src/main/java/com/example/klimaspillet/MainActivity.kt
+++ app/src/main/java/com/example/klimaspillet/MainActivity.kt
@@ -18,9 +18,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.klimaspillet.navigation.Navigation
 import com.example.klimaspillet.ui.screens.ConnectClassScreen
 import androidx.compose.foundation.layout.Box
