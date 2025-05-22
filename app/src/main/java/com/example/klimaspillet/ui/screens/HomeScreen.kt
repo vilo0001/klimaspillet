836e1086f1354728dcfd8248172fc20e38708164
vilo0001 <vilo0001@stud.kea.dk>
2025-05-22 10:41:15 +0200

    En enkelt import som var tabt i merge.

--- app/src/main/java/com/example/klimaspillet/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/klimaspillet/ui/screens/HomeScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
