86d4a15bb4008562f9291214fc0f847e748706b7
vilo0001 <vilo0001@stud.kea.dk>
2025-05-22 10:59:13 +0200

    Ubrugte imports fjernet.

--- app/src/main/java/com/example/klimaspillet/MainActivity.kt
+++ app/src/main/java/com/example/klimaspillet/MainActivity.kt
@@ -2,44 +2,20 @@
 
 package com.example.klimaspillet
 
-import android.content.Context
-import android.media.MediaPlayer
 import android.net.Uri
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.widget.VideoView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.klimaspillet.ui.ViewModel
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import com.example.klimaspillet.navigation.Navigation
-import com.example.klimaspillet.ui.screens.ConnectClassScreen
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import com.example.klimaspillet.navigation.Navigation
-import com.example.klimaspillet.ui.screens.ConnectClassScreen
-import com.example.klimaspillet.ui.theme.KlimaspilletTheme
-import androidx.core.net.toUri
-
 
 
 class MainActivity : ComponentActivity() {
