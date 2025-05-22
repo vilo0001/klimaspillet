d7a2d776034bfb840fcbbb218d0269828da0d216
magnus <magnus.giemsa@gmail.com>
2025-05-22 09:47:15 +0200

    info knap virker ned i HomeScreen.kt og Info knapper til navn og klassekode. Mangler lige en info knao til emoji.

--- app/src/main/java/com/example/klimaspillet/ui/screens/ConnectClassScreen.kt
+++ app/src/main/java/com/example/klimaspillet/ui/screens/ConnectClassScreen.kt
@@ -14,15 +14,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -52,6 +58,7 @@ import com.example.klimaspillet.navigation.Routes
 fun ConnectClassScreen(navController: NavController) {
     var showEmojiPicker by remember { mutableStateOf(false) }
     var selectedEmoji by remember { mutableIntStateOf(R.drawable.emoji1) }
+    BackButton(navController)
 
     Box(
         modifier = Modifier.fillMaxSize()
@@ -168,18 +175,6 @@ fun EmojiButton(emojiId: Int, onClick: () -> Unit) {
 
 
 
-
-// Magnus Giemsa
-@Composable
-fun Background () {
-    Image(
-        painter = painterResource(id = R.drawable.background),
-        contentDescription = "background",
-        contentScale = ContentScale.Crop,
-        modifier = Modifier.fillMaxSize()
-    )
-}
-
 // Tekst knapper (Navn og klassekode)
 //Magnus Giemsa
 @Composable
@@ -194,33 +189,91 @@ fun ClassInputFields() {
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextField(
-            value = name.value,
-            onValueChange = { name.value = it },
-            label = { Text("Navn", fontFamily = FontFamily(Font(R.font.bagel_fat_one)) ) },
-            textStyle = LocalTextStyle.current.copy(
-                fontFamily = FontFamily(Font(R.font.bagel_fat_one))
-            ),
-            singleLine = true,
-            shape = RoundedCornerShape(40.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .shadow(30.dp, RoundedCornerShape(40.dp)),
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = name.value,
+                onValueChange = { name.value = it },
+                label = { Text("Navn", fontFamily = FontFamily(Font(R.font.bagel_fat_one))) },
+                textStyle = LocalTextStyle.current.copy(
+                    fontFamily = FontFamily(Font(R.font.bagel_fat_one))
+                ),
+                singleLine = true,
+                shape = RoundedCornerShape(40.dp),
+                modifier = Modifier
+                    .width(250.dp)
+                    .shadow(30.dp, RoundedCornerShape(40.dp)),
+            )
 
-        TextField(
-            value = classCode.value,
-            onValueChange = { classCode.value = it },
-            label = { Text("Klassekode", fontFamily = FontFamily(Font(R.font.bagel_fat_one))) },
-            textStyle = LocalTextStyle.current.copy(
-                fontFamily = FontFamily(Font(R.font.bagel_fat_one))
-            ),
-            singleLine = true,
-            shape = RoundedCornerShape(40.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .shadow(30.dp, RoundedCornerShape(40.dp))
-        )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            var showDialogName by remember { mutableStateOf(false) }
+
+            Icon(
+                painter = painterResource(id = R.drawable.textinfo),
+                contentDescription = "Info",
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable { showDialogName = true },
+                tint = Color.Black
+            )
+
+            if (showDialogName) {
+                AlertDialog(
+                    onDismissRequest = { showDialogName = false },
+                    text = {
+                        Text("Indtast dit navn (3-15 karakterer)")
+                    },
+                    confirmButton = {}
+                )
+            }
+        }
+
+
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = name.value,
+                onValueChange = { name.value = it },
+                label = { Text("Klassekode", fontFamily = FontFamily(Font(R.font.bagel_fat_one))) },
+                textStyle = LocalTextStyle.current.copy(
+                    fontFamily = FontFamily(Font(R.font.bagel_fat_one))
+                ),
+                singleLine = true,
+                shape = RoundedCornerShape(40.dp),
+                modifier = Modifier
+                    .width(250.dp)
+                    .shadow(30.dp, RoundedCornerShape(40.dp)),
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            var showDialogClass by remember { mutableStateOf(false) }
+
+            Icon(
+                painter = painterResource(id = R.drawable.textinfo),
+                contentDescription = "InfoClass",
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable { showDialogClass = true },
+                tint = Color.Black
+            )
+
+            if (showDialogClass) {
+                AlertDialog(
+                    onDismissRequest = { showDialogClass = false },
+                    text = {
+                        Text("Indtast dit navn (3-15 karakterer)")
+                    },
+                    confirmButton = {}
+                )
+            }
+        }
     }
 }
 
@@ -256,4 +309,27 @@ fun OkButton(navController: NavController) {
         )
     }
 }
-
+@Composable
+fun BackButton(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable {
+                navController.navigate(Routes.routeHomeScreen)
+            }
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.round_arrow_back_ios_24),
+            contentDescription = null,
+            modifier = Modifier
+                .offset(x = 2.dp, y = 1.dp)
+                .blur(3.dp),
+            tint = Color.Gray
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.round_arrow_back_ios_24),
+            contentDescription = "Back",
+            tint = Color.White
+        )
+    }
+}
